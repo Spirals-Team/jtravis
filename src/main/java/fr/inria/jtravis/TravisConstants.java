d0c07ba1c1344ad3fdfaf5ab86901cfc4726277e
André Silva <andre15andre@hotmail.com>
2021-06-24 16:06:09 +0200

    Move default endpoint to travis-ci.com
    Signed-off-by: André Silva <andre15andre@hotmail.com>

--- src/main/java/fr/inria/jtravis/TravisConstants.java
+++ src/main/java/fr/inria/jtravis/TravisConstants.java
@@ -3,7 +3,7 @@ package fr.inria.jtravis;
 public class TravisConstants {
     public static final String TRAVIS_API_V2_ACCEPT_APP = "application/vnd.travis-ci.2+json";
 
-    public static final String TRAVIS_API_ENDPOINT="https://api.travis-ci.org";
+    public static final String TRAVIS_API_ENDPOINT="https://api.travis-ci.com";
     public static final String TRAVIS_COMMERCIAL_API_ENDPOINT="https://api.travis-ci.com";
 
     public static final String TRAVIS_TOKEN_ENV_PROPERTY = "TRAVIS_TOKEN";
